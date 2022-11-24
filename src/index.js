65e6795d383ae11d7f020cef8e300fe2ce9ebdf4
IvanGerman <ivgvi@mail.ru>
2022-11-24 23:34:12 +0700

    fix: use var PORT = process.env.PORT || 3001

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const app = require('./app.js');
-const { PORT } = require('./common/config.js');
+//const { PORT } = require('./common/config.js');
+var PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`Server is running on PORT ${PORT}`);
